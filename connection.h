a258823f4849f30780bdb1e9c2461d38ffee5946
Amine <Challouf.mohamedamine@esprit.tn>
2026-05-03 17:23:43 +0100

    added some design fixes

--- connection.h
+++ connection.h
@@ -12,9 +12,9 @@ static bool createConnection()
     QSqlDatabase db = QSqlDatabase::addDatabase("QOCI");
     db.setHostName("localhost");
     db.setPort(1521);
-    db.setDatabaseName("Source_Projet2A");
-    db.setUserName("RAMII");
-    db.setPassword("rami123");
+    db.setDatabaseName("amine");
+    db.setUserName("amine");
+    db.setPassword("amine14");
 
     if (!db.open()) {
         qDebug() << "QOCI driver failed, trying ODBC...";
@@ -24,7 +24,7 @@ static bool createConnection()
         db = QSqlDatabase::addDatabase("QODBC");
         
         // Option A: Use TNS name (if configured in tnsnames.ora)
-        db.setDatabaseName("Source_Projet2A");
+        db.setDatabaseName("amine");
         
         /* Option B: Full connection string (uncomment if Option A doesn't work)
         db.setDatabaseName(
@@ -35,8 +35,8 @@ static bool createConnection()
         );
         */
         
-        db.setUserName("RAMII");
-        db.setPassword("rami123");
+        db.setUserName("amine");
+        db.setPassword("amine14");
         
         if (!db.open()) {
             qDebug() << "Database connection failed!";
